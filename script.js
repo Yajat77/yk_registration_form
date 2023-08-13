a918cc1fc3261f341daf5561669b06f71145e40a
Yajat77 <113172741+Yajat77@users.noreply.github.com>
2023-08-13 15:22:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const submitButton = document.querySelector('button');
 const dialog = document.getElementById('success-dialog');
 
 submitButton.addEventListener('click', (event) => {
-  event.preventDefault(); // Prevent form submission for this example
+  event.preventDefault(); 
   dialog.style.display = 'block';
 });
